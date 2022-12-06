fffebe1400a0a1eaacd0122cd5daf5f93f1c381c
Jevgeni Rumjantsev <jrumjantsev@box.com>
2022-12-06 10:11:20 +0100

    day06 part 2

--- day06/main.py
+++ day06/main.py
@@ -1,19 +1,23 @@
 from collections import Counter
 
+
+def find_unique_seq_i(data: str, seq_len: int):
+    counter = Counter()
+    for i, char in enumerate(data):
+        counter[char] += 1
+        if i >= seq_len:
+            counter[data[i - seq_len]] -= 1
+            if counter[data[i - seq_len]] == 0:
+                del counter[data[i - seq_len]]
+
+        if len(counter) == seq_len:
+            return i + 1
+    return -1
+
+
 if __name__ == '__main__':
     with open('input.txt') as f:
         line = f.read().strip()
 
-    counter = Counter()
-
-    for i, char in enumerate(line):
-        counter[char] += 1
-        if i >= 4:
-            counter[line[i - 4]] -= 1
-            if counter[line[i - 4]] == 0:
-                del counter[line[i - 4]]
-
-        if len(counter) == 4:
-            print(f"Result 1: {i + 1}")
-            break
-
+    print(f"Result 1: {find_unique_seq_i(line, 4)}")
+    print(f"Result 2: {find_unique_seq_i(line, 14)}")
