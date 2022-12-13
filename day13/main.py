bfb8a601a6a0f81643cf3436ed312c44a3e9a984
Jevgeni Rumjantsev <jrumjantsev@box.com>
2022-12-13 10:53:36 +0100

    day13 part 2

--- day13/main.py
+++ day13/main.py
@@ -6,7 +6,7 @@ def is_arr(x):
     return isinstance(x, collections.abc.Sequence)
 
 def parse_arr(line: str):
-    return eval(line)  # I'm cheating here :)
+    return eval(line)  # I'm cheating here :) TODO: implement parsing
 
 
 def compare_pairs(left: Union[int, List], right: Union[int, List]) -> int:
@@ -21,14 +21,16 @@ def compare_pairs(left: Union[int, List], right: Union[int, List]) -> int:
         return compare_pairs([left], right)
 
     if is_arr(left) and is_arr(right):
+        i = 0
         while True:
-            if not left and not right: return 0
-            if not left and right: return 1
-            if left and not right: return -1
+            if i >= len(left) and i >= len(right): return 0
+            if i >= len(left) and i < len(right): return 1
+            if i < len(left) and i >= len(right): return -1
 
-            result = compare_pairs(left.pop(0), right.pop(0))
+            result = compare_pairs(left[i], right[i])
             if result != 0:
                 return result
+            i += 1
 
     if not is_arr(left) and not is_arr(right):
         return 0 if left == right else 1 if left < right else -1
@@ -36,20 +38,33 @@ def compare_pairs(left: Union[int, List], right: Union[int, List]) -> int:
     raise Exception('should not make it here')
 
 
-
-
 if __name__ == '__main__':
     with open('input.txt') as f:
         lines = [line.strip() for line in f]
 
+    parsed_lines = []
+    for line in lines:
+        if not line: continue
+        parsed_lines.append(parse_arr(line))
+
     part1_result = 0
-    for i in range(0, len(lines), 3):
-        left = parse_arr(lines[i])
-        right = parse_arr(lines[i+1])
-        result = compare_pairs(left, right)
-        # print(f'Left {left} vs Right {right} -> {result}')
-        if result == 1:
-            part1_result += (i//3)+1
+    for i in range(0, len(parsed_lines), 2):
+        left = parsed_lines[i]
+        right = parsed_lines[i+1]
+        if compare_pairs(left, right) == 1:
+            part1_result += (i//2)+1
 
     print(f'Result 1: {part1_result}')
 
+    parsed_lines.extend([[[2]], [[6]]])
+
+    # Bubble sort
+    for i in range(len(parsed_lines)):
+        for j in range(len(parsed_lines)):
+            if compare_pairs(parsed_lines[i], parsed_lines[j]) == -1:
+                parsed_lines[i], parsed_lines[j] = parsed_lines[j], parsed_lines[i]
+    parsed_lines.reverse()
+    x = parsed_lines.index([[2]]) + 1
+    y = parsed_lines.index([[6]]) + 1
+
+    print(f'Result 2: {x * y}')
