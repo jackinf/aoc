42d4f4eedda70c102bb1349599477cdda70d34e3
Jevgeni Rumjantsev <jrumjantsev@box.com>
2022-12-21 20:58:57 +0100

    day20 part1

--- day20/main.py
+++ day20/main.py
@@ -1,94 +1,82 @@
-from pprint import pprint
+from dataclasses import dataclass
+from typing import Optional
 
-import math
+
+@dataclass
+class Node:
+    val: int
+    prev: Optional["Node"]
+    next: Optional["Node"]
+
+
+def print_nodes(node: Node, steps: int):
+    while steps > 0:
+        steps -= 1
+        print(str(node.val), end=', ')
+        node = node.next
+    print()
+
+
+def swap_nodes(left: Node, right: Node):
+    a = left.prev
+    b = right.next
+
+    a.next, right.prev = right, a
+    left.next, b.prev = b, left
+    right.next, left.prev = left, right
+
 
 if __name__ == '__main__':
-    with open('sample.txt') as f:
+    with open('input.txt') as f:
         numbers = [int(line.strip()) for line in f]
-        collection = {order: val for order, val in enumerate(numbers)}
-        indexes = [order for order, _ in enumerate(numbers)]
 
-    pprint(indexes)
-    sign = lambda x: int(math.copysign(1, x))  # two will work
+    N = len(numbers)
+    # print(numbers)
+    head = Node(val=0, prev=None, next=None)
 
-    """
-    1, 2, -3, 3, -2, 0, 4
-    
-    collection:
-    
-        ord: val
-        ----
-        0: 1
-        1: 2
-        2: -3
-        3: 3
-        4: -2
-        5: 0
-        6: 4
-    
-    arr (indexes of numbers):
-        0, 1, 2, 3, 4, 5, 6
-        
-    from 0 to N:
-    steps:
-        0: 
-            collection[0] = 1
-            old: 0, 1, 2, 3, 4, 5, 6
-            new: 1, 0, 2, 3, 4, 5, 6
-        1:  
-            collection[1] = 2
-            old: 1, 0, 2, 3, 4, 5, 6
-            new: 0, 2, 1, 3, 4, 5, 6
-        2:  
-            collection[2] = -3
-            old: 0, 2, 1, 3, 4, 5, 6
-            new: 0, 1, 3, 4, 2, 5, 6
-        ...
-            
-    """
+    queue = []
 
-    N = len(indexes)
+    # construct doubly linked list
+    prev = head
+    while numbers:
+        number = numbers.pop(0)
+        node = Node(val=number, prev=prev, next=None)
+        queue.append(node)
+        prev.next = node
+        prev = node
+    prev.next, head.next.prev = head.next, prev
 
-    for i in range(N):
-        print()
-        print(f'step #{i}:', end=' ')
-        print(', '.join([str(collection[index]) for index in indexes]), end='. ')
+    # print_nodes(head.next, N)
 
-        val = collection[i]
-        print(f'Moving {i}:{val}', end='. ')
+    while queue:
+        node = queue.pop(0)
+        steps = node.val
 
-        if val == 0:
-            continue
+        while steps < 0:
+            steps += 1
+            swap_nodes(node.prev, node)
 
-        # if the number is going to do more cycles than there are numbers in the array
-        if abs(val) > N:
-            val %= N
+        while steps > 0:
+            steps -= 1
+            swap_nodes(node, node.next)
 
-        index_from = indexes.index(i)
-        indexes.pop(index_from)
+        # print_nodes(head.next, N)
 
-        # check if value goes out of bounds when positive
-        if val > 0 and index_from + val >= N:
-            val -= (N-1)
+    # find 0 as starting position
+    node = head.next
+    while node.val != 0:
+        node = node.next
 
-        # check if value goes out of bounds when negative
-        elif val < 0 and index_from < abs(val) + 1:
-            val = (N-1) - abs(val)
+    # find 1000th, 2000th and 3000th elements
+    steps, res = 0, 0
+    while steps <= 3000:
+        node = node.next
+        steps += 1
 
-        index_to = index_from + val
-        indexes.insert(index_to, i)
+        if steps % 1000 == 0:
+            res += node.val
 
-        print('After moving: ', end='')
-        print(', '.join([str(collection[index]) for index in indexes]), end='. ')
-
-    print()
-    numbers = [collection[index] for index in indexes]
-    start_from = numbers.index(0)
-    x1 = collection[indexes[(start_from + 1000) % N]]
-    x2 = collection[indexes[(start_from + 2000) % N]]
-    x3 = collection[indexes[(start_from + 3000) % N]]
-
-    print(f'Result 1: {x1} + {x2} + {x3} = {x1 + x2 + x3}')
+    print(f'Result 1: {res}')
 
 
 
