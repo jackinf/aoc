807d2372002a98da371339b0fbccd51f56f8181b
Jevgeni Rumjantsev <jrumjantsev@box.com>
2023-01-02 14:28:12 +0100

    2022 d01 shorter

--- 2021/day01/main.py
+++ 2021/day01/main.py
@@ -1,22 +1,16 @@
 if __name__ == '__main__':
     with open('input.txt') as f:
         numbers = [int(num.strip()) for num in f]
-    print(numbers)
 
     def count_increases(numbers_):
-        increases = 0
-        for i in range(1, len(numbers_)):
-            if numbers_[i] > numbers_[i-1]:
-                increases += 1
-        return increases
+        return sum(1 for n1, n2 in zip(numbers_, numbers_[1:]) if n1 < n2)
 
     print(f'Result 1: {count_increases(numbers)}')
 
-    numbers2 = numbers[:] + [0, 0]
     sums = numbers[:]
-    for i in range(2, len(numbers2)):
-        sums[i-2] += numbers2[i-1]
-        sums[i-2] += numbers2[i]
+    for i in range(len(numbers) - 2):
+        sums[i] += numbers[i + 1]
+        sums[i] += numbers[i + 2]
     sums = sums[:-2]
 
     print(f'Result 2: {count_increases(sums)}')
