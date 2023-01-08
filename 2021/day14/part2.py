7d2270b7243d125d0cdc01f9123ab7028d54c84e
Jevgeni Rumjantsev <jrumjantsev@box.com>
2023-01-08 17:44:45 +0100

    day14 part 2

--- 2021/day14/part2.py
+++ 2021/day14/part2.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 
 cache = {}
+rules = {}
 
 """
     f(NC, 2) => NBC => f(NB, 1) + f(BC, 1)
@@ -26,58 +27,37 @@ cache = {}
 """
 
 
-def update_counts(counts, letter, sign=1):
-    match letter:
-        case 'N': counts[0] += (1 * sign)
-        case 'B': counts[1] += (1 * sign)
-        case 'C': counts[2] += (1 * sign)
-        case 'H': counts[3] += (1 * sign)
-
-
-def counts_to_str(counts):
-    word = ''
-    word += 'N' * counts[0]
-    word += 'B' * counts[1]
-    word += 'C' * counts[2]
-    word += 'H' * counts[3]
-    return Counter(word)
-
-
-def solve(letter1, letter2, depth, rules):
+def solve(letters, depth):
     if depth == 0:
-        counts_start = [0, 0, 0, 0]
-        update_counts(counts_start, letter1)
-        update_counts(counts_start, letter2)
-        return counts_start
+        return Counter(letters)
 
-    key = (letter1, letter2, depth)
-    if depth in cache:
+    key = (depth, *letters)
+    if key in cache:
         return cache[key]
 
-    new_letter = rules[letter1 + letter2]
+    if len(letters) > 2:
+        left, middle, right = letters[:-1], letters[-2], letters[-2:]
+        counter = solve(left, depth) + solve(right, depth)
+        counter[middle] -= 1
+    elif len(letters) == 2:
+        middle = rules[''.join(letters)]
+        left, right = [letters[0], middle], [middle, letters[1]]
+        counter = solve(left, depth - 1) + solve(right, depth - 1)
+        counter[middle] -= 1
+    else:
+        raise Exception('should not happen')
 
-    counts1 = solve(letter1, new_letter, depth - 1, rules)
-    counts2 = solve(new_letter, letter2, depth - 1, rules)
-
-    # merge counts
-    counts = [0, 0, 0, 0]
-    for i in range(len(counts)):
-        counts[i] += counts1[i]
-        counts[i] += counts2[i]
-    update_counts(counts, new_letter, -1)
-
-    cache[key] = counts
-    return counts
+    cache[(depth, *letters)] = counter
+    return counter
 
 
 if __name__ == '__main__':
-    with open('sample.txt') as f:
+    with open('input.txt') as f:
         lines = [line.strip() for line in f]
 
     template = list(lines.pop(0))
     lines.pop(0)
 
-    rules = {}
     while lines:
         line = lines.pop(0)
         source, dest = line.split('->')
@@ -86,22 +66,11 @@ if __name__ == '__main__':
         rules[source] = dest
 
     counts = [0, 0, 0, 0]
-    # for letter in template:
-    #     update_counts(counts, letter)
 
     steps = 40
-    for i in range(len(template) - 1):
-        counts_ = solve(template[i], template[i+1], steps, rules)
-        print(f'f({template[i]}{template[i+1]}, {steps}) = ', counts_to_str(counts_))
+    final_counter = solve(template, steps)
 
-        for i in range(len(counts)):
-            counts[i] += counts_[i]
+    mc = final_counter.most_common()
+    res = mc[0][1] - mc[-1][1]
+    print(f'Result 2: {res}')
 
-    for i in range(1, len(template) - 1):
-        update_counts(counts, template[i], -1)
-
-    print(f"FINAL: f({''.join(template)}, {steps}) = ", counts_to_str(counts))
-    # print('final', counts)
-    print(f'Min: {min(counts)}, Max: {max(counts)}, Result: {max(counts) - min(counts)}')
-
-    # print(f'Result 2: {res}')
