727d74023ec5f25711b97da8418e036dc18a5b1d
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2023-12-04 11:54:33 +0100

    2023 day 04 part 1 one-liner

--- 2023/day04/part1.py
+++ 2023/day04/part1.py
@@ -8,3 +8,7 @@ for line in lines:
     total += 2 ** (matches - 1) if matches > 0 else 0
 
 print(f'Part 1: {total}')
+
+# Alternative solution for lolz
+total_alternative = sum(2 ** (x - 1) for x in [len(set(cards[0]) & set(cards[1])) for cards in [[cards.split() for cards in line.split(':')[1].split('|')] for line in lines]] if x > 0)
+print(f'Part 1 (alternative): {total}')
