4e50445ba9d03bc0ec29f468111ec4a938b531fa
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2023-12-11 20:38:01 +0100

    2023 day11 part1 part2

---
+++ 2023/day11/part2.py
@@ -0,0 +1,63 @@
+from typing import Dict, Tuple
+from collections import defaultdict
+
+
+with open('input.txt', 'r') as f:
+    grid = [list(line) for line in f.read().split('\n')]
+
+
+def rotate_90deg(grid):
+    return list(zip(*grid[::-1]))  # cool trick to rotate 2d array 90 deg
+
+
+# expand galaxies
+empty_rows = [i for i, row in enumerate(grid) if all(col == '.' for col in row)]
+
+grid = rotate_90deg(grid)
+empty_cols = [i for i, row in enumerate(grid) if all(col == '.' for col in row)]
+
+# rotate back into place
+grid = rotate_90deg(grid)
+grid = rotate_90deg(grid)
+grid = rotate_90deg(grid)
+
+# find all galaxy coordinates
+galaxies_arr = []
+
+for row in range(len(grid)):
+    for col in range(len(grid[0])):
+        if grid[row][col] == '#':
+            galaxies_arr.append((row, col))
+galaxies = { v: i for i, v in enumerate(galaxies_arr)}
+galaxies_reversed = { v: i for i, v in enumerate(galaxies_arr)}
+
+# for each galaxy, BFS till all scanned
+pairs: Dict[Tuple[int, int], int] = defaultdict(int)
+for i, (start_row, start_col) in enumerate(galaxies):
+    print(f'Galaxy {i + 1} of {len(galaxies_arr)}')
+    seen = set()
+    q = [(start_row, start_col, 0)]
+    while q:
+        row, col, depth = q.pop(0)
+        if not (0 <= row < len(grid) and 0 <= col < len(grid[0])):
+            continue
+        if (row, col) in seen:
+            continue
+        seen.add((row, col))
+
+        if grid[row][col] == '#' and (row, col) in galaxies_reversed:
+            j = galaxies_reversed[(row, col)]
+            if i != j:
+                lower, higher = min(i, j), max(i, j)
+                pairs[(lower, higher)] = depth
+
+        for delta_row, delta_col in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            new_row, new_col = row + delta_row, col + delta_col
+
+            if new_row in empty_rows or new_col in empty_cols:
+                q.append((new_row, new_col, depth + 1000000))
+            else:
+                q.append((new_row, new_col, depth + 1))
+
+result = sum(pairs.values())
+print(f'Part 1: {result}')
