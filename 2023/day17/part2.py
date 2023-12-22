0a103f2f58a7fe27a9b82d4c863042b8d426ab2c
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2023-12-22 21:22:43 +0100

    2023 day17 part 2

--- 2023/day17/part2.py
+++ 2023/day17/part2.py
@@ -12,7 +12,6 @@ def debug(grid, seen):
     for row in range(len(grid)):
         print()
         for col in range(len(grid[0])):
-
             if (row, col) in seen:
                 print(str(grid[row][col]), end='')
             else:
@@ -22,12 +21,14 @@ def debug(grid, seen):
 seen = set()
 best_row, best_col = 0, 0
 q = []
-COUNTER = 0
-q.append((0, 0, 0, (0, 0), COUNTER))
+
+# heat, row, col, delta row, delta col, move_counter
+q.append((0, 0, 0, 0, 0, 4, []))
 moves = 0
 while q:
     moves += 1
-    heat, row, col, dir, counter = heapq.heappop(q)
+    heat, row, col, drow, dcol, counter, path = heapq.heappop(q)
+    path.append((row, col))
 
     if is_oob(grid, row, col):
         continue
@@ -37,12 +38,13 @@ while q:
     best_col = max(best_col, col)
     print(f'q len = {len(q)}, best_row = {best_row} / {len(grid)}, best_col = {best_col} / {len(grid[0])}')
 
-    if row == len(grid) - 1 and col == len(grid[0]) - 1:
-        print(f'Part 1: {heat + 1}, moves: {moves}')  # i am not 100% sure why i need 1 extra step
+    if row == len(grid) - 1 and col == len(grid[0]) - 1 and counter >= 4:
+        debug(grid, path)
+        print(f'Part 2: {heat + 1}, moves: {moves}')
         break
 
     # optimization
-    key = (row, col, dir[0], dir[1], counter)
+    key = (row, col, drow, dcol, counter)
     if key in seen:
         continue
     seen.add(key)
@@ -51,13 +53,14 @@ while q:
     new_heat = heat + grid[row][col]
 
     # move forward
-    if counter < 3 and (dir[0], dir[1]) != (0, 0):
-        heapq.heappush(q, (new_heat, row + dir[0], col + dir[1], dir, counter + 1))
+    if counter < 10 and (drow, dcol) != (0, 0):
+        heapq.heappush(q, (new_heat, row + drow, col + dcol, drow, dcol, counter + 1, path[:]))
 
     # turn
-    for new_dr, new_dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        # do not go forwards or backwards
-        if (new_dr, new_dc) in ((dir[0], dir[1]), (-dir[0], -dir[1])):
-            continue
+    if counter >= 4:
+        for new_dr, new_dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            # do not go forwards or backwards
+            if (new_dr, new_dc) in ((drow, dcol), (-drow, -dcol)):
+                continue
 
-        heapq.heappush(q, (new_heat, row + new_dr, col + new_dc, (new_dr, new_dc), 1))
+            heapq.heappush(q, (new_heat, row + new_dr, col + new_dc, new_dr, new_dc, 1, path[:]))
