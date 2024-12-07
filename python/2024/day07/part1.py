728796b7061c9f8cc80fd2b3a065ac27e91a0b70
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2024-12-07 08:57:20 +0100

    2024 day 7 part 1

---
+++ python/2024/day07/part1.py
@@ -0,0 +1,25 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+final_result = 0
+for i, line in enumerate(lines):
+    left, right = line.split(':')
+    left = int(left)
+    right = list(map(int, right.split()))
+
+    q = [(1, right[0])]
+    while q:
+        right_index, ans = q.pop(0)
+        if ans > left:
+            continue
+
+        if right_index == len(right):
+            if ans == left:
+                final_result += ans
+                break
+            continue
+
+        q.append((right_index + 1, ans + right[right_index]))
+        q.append((right_index + 1, ans * right[right_index]))
+
+print(f'Part 1: {final_result}')
