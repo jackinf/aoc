760fef460efce30b94ed9ae5c6946fed7b5ba717
Jevgeni Rumjantsev <jrumjantsev@box.com>
2024-12-19 10:51:47 +0100

    2024 day 19 part 1

---
+++ python/2024/day19/part1.py
@@ -0,0 +1,37 @@
+from functools import cache
+
+
+def read_input():
+    with open('input.txt') as f:
+        blocks = f.read().split('\n\n')
+        patterns = blocks[0].split(', ')
+        designs = blocks[1].split('\n')
+
+    return set(patterns), designs
+
+
+def run():
+    patterns, designs = read_input()
+
+    @cache
+    def check_match(design: str) -> bool:
+        if design == '':
+            return True
+
+        for pattern in patterns:
+            if design.startswith(pattern):
+                if check_match(design[len(pattern):]):
+                    return True
+
+        return False
+
+    matches = 0
+    for design_index, design_val in enumerate(designs):
+        print(f"Design '{design_val}': {design_index + 1} out of {len(designs)}")
+        if check_match(design_val):
+            matches += 1
+
+    print(f'Part 1: {matches}')
+
+
+run()
