f2271f8178303f2a68bcca4d16c1aafa735beec3
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2024-12-24 15:19:43 +0100

    2024 day 9 part 2 wrong answer i give up

--- python/2024/day09/part2.py
+++ python/2024/day09/part2.py
@@ -19,29 +19,25 @@ def parse_line(line):
             spaces[id] = int(line[index])
             index += 1
 
-    # Add initial space block
-    # spaces[-1] = 0
     return files, spaces
 
 def step(files, spaces, results) -> bool:
     for file_id in sorted(files.keys(), reverse=True):
         for spaces_id in sorted(spaces.keys(), reverse=False):
-            # print(f'Spaces id: {spaces_id}, File id: {file_id}')
             if files[file_id] <= spaces[spaces_id]:
-                # print(f'File {file_id} moved into {spaces_id}')
                 spaces[spaces_id] -= files[file_id]
                 results[spaces_id].append((file_id, files[file_id]))
 
                 del files[file_id]
-                # if spaces[spaces_id] == 0:
-                #     del spaces[spaces_id]
 
                 return False
 
     return True
 
 def solve(files, spaces):
-    results = defaultdict(list)
+    results = defaultdict(list) # collect all the movements of the file here
+
+    # as we will modify the arrays (by subtracting or removing values)
     files2 = files.copy()
     spaces2 = spaces.copy()
 
@@ -51,30 +47,30 @@ def solve(files, spaces):
         if done:
             break
 
-    result_str = ''
+    # construct the answer
+    result = []
     for file_id in sorted(files2.keys(), reverse=False):
-        if file_id in files:
-            result_str += (str(file_id) * files[file_id])
-        else:
-            result_str += ('.' * files2[file_id])
+        val = str(file_id) if file_id in files else '.'
+        for _ in range(files2[file_id]):
+            result.append(val)
 
-        occupied_spaces = 0
+        left_spaces = spaces2[file_id]
         for k, v in results[file_id]:
-            result_str = result_str + (str(k) * v)
-            occupied_spaces += v
+            for _ in range(v):
+                result.append(str(k))
+            left_spaces -= v
 
-        left_spaces = spaces2[file_id] - occupied_spaces
-        result_str += '.' * left_spaces
+        for _ in range(left_spaces):
+            result.append('.')
 
-    return result_str
+    return result
 
 def calculate_final_result(result):
     final_result = sum((0 if k == '.' else int(k)) * i for i, k in enumerate(result))
     return final_result
 
 def main():
-    # Change the file path as needed
-    file_path = 'input.txt'
+    file_path = 'sample1.txt'
     line = read_input_file(file_path)
 
     files, spaces = parse_line(line)
@@ -89,3 +85,4 @@ if __name__ == "__main__":
     main()
 
 # 95174136247 - too low
+# 8456900406063 - too high
