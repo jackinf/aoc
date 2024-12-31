afcfd2063d181163381062ec850ecb31b9239a1f
Jevgeni Rumjantsev <jrumjantsev@box.com>
2024-12-31 11:37:46 +0100

    2024 day 20 part 2

--- python/2024/day20/part2.py
+++ python/2024/day20/part2.py
@@ -1,106 +1,60 @@
-from collections import defaultdict, Counter
+import collections
 
 WALL = '#'
-EMPTY = '.'
-STEP_COST = 1
 START = 'S'
 END = 'E'
 
-def count_walls(grid):
-    wall_count = 0
-    for row in range(1, len(grid) - 1):
-        for col in range(1, len(grid[0]) - 1):
-            if grid[row][col] == WALL:
-                wall_count += 1
-    return wall_count
-
-def find_symbol(grid, symbol):
-    for row in range(1, len(grid) - 1):
-        for col in range(1, len(grid[0]) - 1):
-            if grid[row][col] == symbol:
-                return row, col
-    raise Exception('not found')
-
-def read_input():
-    with open('sample1.txt') as f:
+def read_input(file_name: str):
+    with open(file_name) as f:
         return [list(line) for line in f.read().split('\n')]
 
 def oob(grid, row, col):
     return not (0 <= row < len(grid) and 0 <= col < len(grid[0]))
 
-def traverse(grid, start, end, cut_limit: int):
-    q = [(0, start, set())]
+def get_path(grid):
+    start = next((i, j) for i, row in enumerate(grid) for j, cell in enumerate(row) if cell == START)
+    end = next((i, j) for i, row in enumerate(grid) for j, cell in enumerate(row) if cell == END)
 
-    seen = set()
+    print(start, end)
 
-    results = []
-    while q:
-        steps, curr, cuts = q.pop(0)
-        row, col = curr
+    # X - row, Y - col
+    px, py = start
+    cx, cy = start
+    while grid[cx][cy] != END:
+        yield cx, cy
 
-        cache_key = tuple(cuts) + (curr,)
-        if cache_key in seen:
-            continue
-        seen.add(cache_key)
+        tx, ty = cx, cy
+        cx, cy = next((nx, ny) for nx, ny in ((cx + 1, cy), (cx - 1, cy), (cx, cy + 1), (cx, cy - 1))
+                      if grid[nx][ny] != WALL and (nx, ny) != (px, py))
+        px, py = tx, ty
 
-        if curr == end:
-            results.append(steps)
-            continue
-
-        for nei in ((row-1, col), (row+1, col), (row, col-1), (row, col+1)):
-            nrow, ncol = nei
-
-            if oob(grid, nrow, ncol):
-                continue
-
-            if grid[nrow][ncol] == EMPTY:
-                q.append((steps + STEP_COST, nei, cuts))
-                continue
-
-            if grid[nrow][ncol] == WALL:
-                if len(cuts) == cut_limit:
-                    continue
-
-                if (nrow, ncol) in cuts:
-                    continue
-
-                cuts2 = set(cuts)
-                cuts2.add((nrow, ncol))
-                q.append((steps + STEP_COST, nei, cuts2))
-
-    return results
-
-def analyze_counter(counter, normal_score):
-    for k, v in sorted(counter.items(), key=lambda x: x[0], reverse=True):
-        saved = normal_score - k
-        print(f'There are {v} cheats that save {saved} picoseconds.')
+    yield end
 
 
-def debug_grid(grid, marks):
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if (row, col) in marks:
-                print('O', end='')
-            else:
-                print('.' if grid[row][col] == WALL else ' ', end='')
-        print()
+def walk(path, max_allowed_cut):
+    # calculate all the pairs in the path
+    for i1, (row1, col1) in enumerate(path):
+        for i2 in range(i1 + 1, len(path)):
+            row2, col2 = path[i2]
+            shortcut_distance = abs(row1 - row2) + abs(col1 - col2)
+            normal_distance = i2 - i1  # what is the normal travel distance
+            saved = normal_distance - shortcut_distance    # how much steps did we save
+
+            # did we save anything at all, and if so, is the shortcut within allowed 100 steps
+            if saved > 0 and shortcut_distance <= max_allowed_cut:
+                yield saved
+
 
 def run():
-    grid = read_input()
+    grid = read_input("input.txt")
+    path = list(get_path(grid))
     print(f'Grid size M x N: {len(grid) * len(grid[0])}')
+    print(path)
 
-    start = find_symbol(grid, START)
-    grid[start[0]][start[1]] = EMPTY
-    end = find_symbol(grid, END)
-    grid[end[0]][end[1]] = EMPTY
+    scores = list(walk(path, 20))
+    final_score = sum(score >= 100 for score in scores)
+    print(collections.Counter(scores))
+    print(f'Part 2: {final_score}')
 
-    normal_results = traverse(grid, start, end, 0)
-    print(f'normal_results: {normal_results}')
-
-    # no idea... i realized that as the maze is narrow, then there's no need for a-star
-    # however now i don't know how find so many combinations
-
-    cheat_score = traverse(grid, start, end, 1)
-    print(f'cheat_score: {cheat_score}')
-
-run()
+if __name__ == '__main__':
+    run()
