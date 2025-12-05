eb36119b35b2de75e0c8a40e4889a4bebdbf4f03
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2025-12-05 07:50:34 +0100

    aoc rust 2025 day 5 part 1

---
+++ rust/bin/2025/day05/part1.rs
@@ -0,0 +1,80 @@
+use log::{debug, info};
+use std::collections::VecDeque;
+
+fn main() {
+    env_logger::init();
+
+    let contents = include_str!("input.txt");
+    let groups: Vec<&str> = contents.split("\n\n").collect();
+    let group0_raw = groups[0].split("\n").collect::<Vec<&str>>();
+    let group1_raw = groups[1].split("\n").collect::<Vec<&str>>();
+
+    let mut ranges = group0_raw.into_iter()
+        .map(|item| {
+            let parts = item.split("-").collect::<Vec<&str>>();
+            let left = parts[0].parse::<u128>().unwrap();
+            let right = parts[1].parse::<u128>().unwrap();
+
+            (left, right)
+        })
+        .collect::<Vec<(u128, u128)>>();
+
+    let mut candidates = group1_raw.iter()
+        .map(|item| item.parse::<u128>().unwrap())
+        .collect::<Vec<u128>>();
+
+    ranges.sort();
+    candidates.sort();
+
+    // debug!("{:?}", ranges);
+    // debug!("{:?}", candidates);
+
+    // merge ranges
+    let mut deque: VecDeque<(u128, u128)> = VecDeque::new();
+    for (s1, e1) in ranges {
+        if deque.is_empty() {
+            deque.push_back((s1, e1));
+            continue;
+        }
+
+        let (s2, e2) = deque.back_mut().unwrap();
+
+        if *e2 >= s1 {
+            *e2 = e1.max(*e2);
+            continue;
+        }
+
+        if *s2 >= s1 {
+            panic!("Should not happen");
+        }
+
+        if *e2 < s1 {
+            deque.push_back((s1, e1));
+            continue;
+        }
+    }
+
+    debug!("{:?}", deque);
+
+    let mut result = 0;
+
+    for id in candidates {
+        while !deque.is_empty() {
+            let (start, end) = deque.front().unwrap();
+            
+            if id < *start {
+                break;
+            }
+
+            if id > *end {
+                deque.pop_front();
+                continue;
+            }
+
+            result += 1;
+            break;
+        }
+    }
+
+    info!("{result}");
+}
