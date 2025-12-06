ec2a69318c9c3f9cb1a429b8654454c08876713b
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2025-12-06 17:07:07 +0100

    aoc rust 2025 day 6 part 2

--- rust/bin/2025/day06/part2.rs
+++ rust/bin/2025/day06/part2.rs
@@ -1,57 +1,79 @@
 use log::{debug, info};
-use std::collections::VecDeque;
 
 fn main() {
     env_logger::init();
 
-    let contents = include_str!("sample0.txt");
-    let groups: Vec<&str> = contents.split("\n\n").collect();
-    let group0_raw = groups[0].split("\n").collect::<Vec<&str>>();
+    let contents = include_str!("input.txt");
+    let grid: Vec<Vec<char>> = contents
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+    // debug!("GRID = {:?}", grid);
 
-    let mut ranges = group0_raw.into_iter()
-        .map(|item| {
-            let parts = item.split("-").collect::<Vec<&str>>();
-            let left = parts[0].parse::<u128>().unwrap();
-            let right = parts[1].parse::<u128>().unwrap();
-
-            (left, right)
-        })
-        .collect::<Vec<(u128, u128)>>();
-
-    ranges.sort();
-
-    // merge ranges
-    let mut deque: VecDeque<(u128, u128)> = VecDeque::new();
-    for (s1, e1) in ranges {
-        if deque.is_empty() {
-            deque.push_back((s1, e1));
-            continue;
-        }
-
-        let (s2, e2) = deque.back_mut().unwrap();
-
-        if *e2 >= s1 {
-            *e2 = e1.max(*e2);
-            continue;
-        }
-
-        if *s2 >= s1 {
-            panic!("Should not happen");
-        }
-
-        if *e2 < s1 {
-            deque.push_back((s1, e1));
-            continue;
-        }
+    let last_row = grid.len() - 1;
+    let mut op_indices_start: Vec<usize> = vec![];
+    let mut op_indices_end: Vec<usize> = vec![];
+    for col in 0..grid[0].len() {
+        // let col_len = grid[0].len();
+        // debug!("OP INDEXES. COL = {col} COL_LEN = {col_len}");
+        let therow = &grid[last_row];
+        let val = therow[col];
+        // debug!("VAL = {val}");
+        if val != ' ' {
+            op_indices_start.push(col);
+            if col >= 1 {
+                op_indices_end.push(col - 1);
+            }
+        } 
     }
+    op_indices_end.push(grid[0].len() - 1);
 
-    debug!("{:?}", deque);
+    assert_eq!(op_indices_start.len(), op_indices_end.len());
+    
+    // debug!("START OP INDICES = {:?}", op_indices_start);
+    // debug!("END OP INDICES = {:?}", op_indices_end);
 
-    let mut result = 0;
+    let mut result: u64 = 0;
 
-    for (start, end) in deque {
-        result += end - start + 1;
+    for op_index in 0..op_indices_start.len() {
+        let op_index_start = op_indices_start[op_index];
+        let op_index_end = op_indices_end[op_index];
+
+        let mut numbers: Vec<u64> = vec![];
+        for col in (op_index_start..=op_index_end).rev() {
+            let mut number_chars: Vec<char> = vec![];
+            for row in 0..grid.len() - 1 {
+                // debug!("ROW {row} AND COL {col}");
+                if grid[row][col] != ' ' {
+                    number_chars.push(grid[row][col]);
+                }
+            }
+
+            // if all spaces collected
+            if number_chars.is_empty() {
+                continue;
+            }
+
+            // debug!("number_chars = {number_chars:?}");
+            let number_str: String = number_chars.iter().collect();
+            // debug!("number_str = {number_str}");
+            let number = number_str.parse::<u64>().expect("should have collected a valid number");
+            // debug!("number = {number}");
+            numbers.push(number);
+        }
+
+        info!("COLLECTED = {numbers:?}");
+
+        if grid[grid.len() - 1][op_index_start] == '*' {
+            let product: u64 = numbers.iter().product();
+            debug!("PRODUCT = {product}");
+            result += product;
+        } else if grid[grid.len() - 1][op_index_start] == '+' {
+            let sum: u64 = numbers.iter().sum();
+            debug!("SUM = {sum}");
+            result += sum;
+        }
+
+        info!("RESULT = {result}");
     }
-
-    info!("{result}");
 }
